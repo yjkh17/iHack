c7f815c32068d69a5b526b4f6f8bfd29950268e0
yjkh17 <yj.kh17@gmail.com>
2025-05-28 19:19:35 +0300

    Initial Commit

---
+++ iHack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iHack
+//
+//  Created by Yousef Jawdat on 28/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iHack/iHackApp.swift
@@ -0,0 +1,17 @@
+//
+//  iHackApp.swift
+//  iHack
+//
+//  Created by Yousef Jawdat on 28/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct iHackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
